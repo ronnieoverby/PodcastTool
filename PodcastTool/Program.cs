c4bafc70b67b2b423743dbf9a26aafb46f31b399
Ronnie Overby <roverby@core-techs.net>
2011-11-22 16:44:21 -0500

    init commit added ref to naudio

---
+++ PodcastTool/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using NAudio.Wave;
+
+namespace PodcastTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            // check for passed in argument
+            if (args == null || !args.Any()) return;
+
+            // check file exists
+            string filePath = args[0];
+            if (!File.Exists(filePath)) return;
+
+        
+        }
+    }
+}
